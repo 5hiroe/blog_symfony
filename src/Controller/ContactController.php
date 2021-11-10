3bc46ca14f3aa3ca962faeabb5ecdf5ebba86f4d
Shiroe <n.caruell@icloud.com>
2021-11-10 09:01:46 +0100

    Initial commit

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+
+
+
+    /**
+     * @Route("/contact/{city}", name="contact")
+     */
+    public function index(Request $request, string $city = ""): Response
+    {
+        $customerName = $request->get('customerName');
+
+        return $this->render('contact/index.html.twig', [
+            'customerName' => $customerName,
+            'city' => $city,
+        ]);
+    }
+}
